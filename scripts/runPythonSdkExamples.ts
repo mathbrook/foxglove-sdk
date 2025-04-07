8b8b53cca7271dd97ee5acf00bfee7f1c30a219f
Bryan Fox <39674+bryfox@users.noreply.github.com>
2025-04-07 09:49:27 -0400

    python: use published SDK for examples (#314)
    ### Changelog
    
    python: update examples to use the published package
    
    ### Description
    
    We no longer require local build environments to run these python
    examples.

--- scripts/runPythonSdkExamples.ts
+++ scripts/runPythonSdkExamples.ts
@@ -8,6 +8,10 @@ import path from "node:path";
 /**
  * Run each example in the Python SDK, after installing dependencies.
  *
+ * If `--install-sdk-from-path` is passed, then the project dependencies will be updated to refer
+ * to the SDK at a local path, relative to the example directory. CI uses this to test with the
+ * latest SDK; by default, examples specify the published version in their pyproject.toml.
+ *
  * Many of the examples start a live server which is run until interrupted; all examples are run
  * with a timeout (default 5s). These are run serially since they use the default Foxglove port
  * number and, for simplicity, don't illustrate that configuration.
@@ -17,10 +21,11 @@ const pyExamplesDir = path.resolve(__dirname, "../python/foxglove-sdk-examples")
 
 const tempFiles: string[] = [];
 
-async function main({ timeout }: { timeout: string }) {
+async function main(opts: { timeout: string; installSdkFromPath: boolean }) {
   for (const example of await readdir(pyExamplesDir)) {
-    await installDependencies(example);
-    await runExample(example, parseInt(timeout));
+    console.debug(`Install & run example ${example}`);
+    await installDependencies(example, { installSdkFromPath: opts.installSdkFromPath });
+    await runExample(example, parseInt(opts.timeout));
   }
 }
 
@@ -48,10 +53,11 @@ async function runExample(name: string, timeoutMillis = 5000) {
   });
 }
 
-async function installDependencies(name: string) {
+async function installDependencies(name: string, opts: { installSdkFromPath: boolean }) {
   const dir = path.join(pyExamplesDir, name);
   return await new Promise((resolve, reject) => {
-    const child = spawn("poetry", ["install"], {
+    const args = opts.installSdkFromPath ? ["add", "foxglove-sdk@../../foxglove-sdk"] : ["install"];
+    const child = spawn("poetry", args, {
       cwd: dir,
     });
     child.stdout.on("data", (data: Buffer | string) => {
@@ -104,6 +110,7 @@ async function extraArgs(example: string) {
 
 program
   .option("--timeout [duration]", "timeout for each example in milliseconds", "5000")
+  .option("--install-sdk-from-path", "use local sdk instead of version from pyproject", false)
   .action(main)
   .hook("postAction", removeTempFiles)
   .parse();
